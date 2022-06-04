8f6c1e8af7133062f7bdcbf0870d6b39b6591ffd
Paloma Dias <106889888+HellenaDiana@users.noreply.github.com>
2022-06-04 23:53:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+operadores = 10 <= 10
+
+print(operadores)
+
+
+
+
+#     ==  Equal
+#     !=  Not equal
+#     >   Greater than
+#     <   Less than 
+#     >=  Greater than or equal to
+#     <=  Less than or equal to
